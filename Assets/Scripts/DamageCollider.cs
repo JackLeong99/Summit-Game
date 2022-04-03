5ff6dd87dccfe0f87f54695bfcb6674b88583484
GarethDickson <53723583+GarethDickson@users.noreply.github.com>
2022-04-03 19:48:31 +1000

    backupDamage

---
+++ Assets/Scripts/DamageCollider.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Windows.Forms.dll;
+using UnityEngine;
+
+public class DamageCollider : MonoBehaviour
+{
+    Collider damageCollider;
+    public int currentWeaponDamage = 50;
+
+    private void Awake(){
+        damageCollider = GetComponent<Collider>();
+        damageCollider.gameObject.SetActive(true);
+        damageCollider.isTrigger = true;
+        damageCollider.enabled = false;
+    }
+
+    public void EnableDamageCollider(){
+        damageCollider.enabled = true;
+    }
+
+    public void DisableDamageCollider(){
+        damageCollider.enabled = false;
+    }
+
+    private void OnTriggerEnter(Collider collision){
+        if(collision.tag == "Hittable"){
+            HealthTemp healthTemp = collision.GetComponent<HealthTemp>();
+
+            if (healthTemp != null){
+                healthTemp.takeDamage(currentWeaponDamage);
+            }
+        }
+    }
+
+    private void ToggleDamageCollider(object sender, System.Windows.Forms.KeyEventArgs e){
+        if(e.KeyCode == Keys.Enter){
+            if (damageCollider.enabled == false){
+                EnableDamageCollider();
+                return;
+            }
+            DisableDamageCollider();
+        }
+    }
+}
