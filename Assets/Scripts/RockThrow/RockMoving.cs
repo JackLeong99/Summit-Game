25cf718df82a23d2872cc128e0bc46ba92319440
coolevil98 <odinbraad@gmail.com>
2022-04-23 16:36:09 +1000

    Rock throw works just needs to set it on boss

--- Assets/Scripts/RockThrow/RockMoving.cs
+++ Assets/Scripts/RockThrow/RockMoving.cs
@@ -6,23 +6,28 @@ public class RockMoving : MonoBehaviour
 { 
     //private Transform target;
     public Transform target;
+    //target will be converted to player
     private float translationZ;
     private float translationY;
     private float translationX;
     private float speed=20;
+    private float rageMultiplier=1;
 
         private float trackZ;
     private float trackY;
     private float trackX;
     private Vector3 localArea;
     public GameObject rockPrefab;
+
+    private GameObject player;
     // Start is called before the first frame update
     void Start()
     {
-       // GameObject player = GameObject.Find("Player"); //probably can do this in boss that then sends the values needed to here.
+        //GameObject test=GameObject.FindWithTag("Player"); or
+       // GameObject player = GameObject.Find("Player"); //later boss script can send this value to this scripts
         //target=player.transform;
-
-
+        GameObject player=GameObject.FindWithTag("Target");
+        target=player.transform;
         //puts position into local space
         localArea= transform.InverseTransformPoint(target.position); 
         localArea.Normalize();
@@ -35,7 +40,10 @@ public class RockMoving : MonoBehaviour
     // Update is called once per frame
    void Update()
     {
-        transform.Translate(translationX *(speed* Time.deltaTime), translationY *(speed* Time.deltaTime), translationZ *(speed* Time.deltaTime));
+        transform.Translate(translationX *(speed*rageMultiplier * Time.deltaTime), translationY *(speed*rageMultiplier* Time.deltaTime), translationZ *(speed*rageMultiplier * Time.deltaTime));
+       
+       
+       //if(transform.position.y<=ground level) set a ground level for it to spawn at
         if(transform.position.y<=target.transform.position.y)
         {
             Debug.Log("hi");
@@ -44,9 +52,31 @@ public class RockMoving : MonoBehaviour
         }
     }
 
-    public void spawnRock(){
+    //spawn ground rock and destroy itself
+    public void spawnRock()
+    {
         GameObject breakablerock= Instantiate(rockPrefab); 
         breakablerock.transform.position=target.position;
         Destroy(gameObject);
+    }
+    
+
+    //this will be called to enable rage ability
+    public void EnableRageMode()
+    {
+        rageMultiplier=2;
+    }
+
+    void OnCollisionEnter(Collision collider)
+    {
+        GameObject other = collider.gameObject;
+
+        if(other==player)
+        {
+            HealthTemp healthTemp = other.GetComponent<HealthTemp>();
+            healthTemp.takeDamage(10);
+            //damage player
+            //takeDamage(some value) from health temp or where ever
+        }
     }
 }
--- Assets/Scripts/RockThrow/RockPickedUp.cs
+++ Assets/Scripts/RockThrow/RockPickedUp.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 
 public class RockPickedUp : MonoBehaviour
 {
-    private float speed=10f;
+    private float speed=0f; //set to 10 for testing purposes
+    public GameObject rockPrefab; //on moving rock
     // Start is called before the first frame update
     void Start()
     {
@@ -16,19 +17,27 @@ public class RockPickedUp : MonoBehaviour
     {
         //call pickup when it is going to be thrown in air 
         //ie it can be damaged before it is thrown
+        transform.Translate(0,speed* Time.deltaTime ,0);
+
+        if(transform.position.y>=7) //y can be changed to any height
+        {
+            ReachedTop();
+        }
     }
 
     //when it reaches the top it will be destroyed and a moving rock will be spawned
     public void ReachedTop()
     {
         //spawns rock for throwing
+        GameObject thrownRock= Instantiate(rockPrefab); 
+        thrownRock.transform.position=transform.position;
         Destroy(gameObject);
 
     }
 
-    //code for the rock moving up for when it is picked up
+    //code for the rock moving up for when it is "picked up"
     public void PickedUp()
     {
-        transform.Translate(0,speed* Time.deltaTime ,0);
+        speed=10f;
     }
 }
