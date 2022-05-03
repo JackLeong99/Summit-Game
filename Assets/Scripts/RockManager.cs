883179e1f2d98544694412c10a15df2de47822f2
coolevil98 <odinbraad@gmail.com>
2022-05-03 11:39:13 +1000

    created code to find closes rock

--- Assets/Scripts/RockManager.cs
+++ Assets/Scripts/RockManager.cs
@@ -22,4 +22,39 @@ public class RockManager : MonoBehaviour
     {
         
     }
+
+    public void RockPositionUpdate(int postionInArray)
+    {
+        //update the array with the new position
+    }
+
+    public GameObject FindClosesRock(Transform bossPos) //needs to give pos of boss
+    {
+        //only need x and z
+
+        //GameObject closest;
+        int closesRock=0;
+
+        float lowestValue=10000;
+
+
+        for(int i=0; i<allRocks.Count; i++){
+            float tempx=allRocks[i].transform.position.x-bossPos.position.x;
+            float tempz=allRocks[i].transform.position.z-bossPos.position.z;
+            float distanceFromBoss=(tempx*tempx)+(tempz*tempz);
+
+            if(distanceFromBoss<lowestValue)
+            {
+                lowestValue=distanceFromBoss;
+                closesRock=i;
+            }
+
+
+            //x2 + z2 find smallest
+        }
+
+        GameObject closest=allRocks[closesRock];
+        return closest;
+        //need to return something
+    }
 }
