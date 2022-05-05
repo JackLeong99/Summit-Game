01dd62f173167548e9e549d8f422e2668a05e215
coolevil98 <odinbraad@gmail.com>
2022-05-05 15:08:22 +1000

    Boss goes toward the rock first and then throws

--- Assets/Scripts/RockThrow & Stomp/RockPathFinding.cs
+++ Assets/Scripts/RockThrow & Stomp/RockPathFinding.cs
@@ -5,7 +5,9 @@ using UnityEngine;
 public class RockPathFinding : MonoBehaviour
 {
     private GameObject target;
-   // public RockManager rockManager;
+    private RockPickedUp targeting;
+    private bool currentlyTargeting=false;
+    private int MoveSpeed = 4;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,18 +17,35 @@ public class RockPathFinding : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-                if(Input.GetButtonDown("Teleport"))
-    {
-        SetTarget();
+        if(Input.GetButtonDown("Teleport"))
+        {
+            SetTarget();
+        }
+        if(currentlyTargeting)
+        {
+        transform.position += transform.forward * MoveSpeed * Time.deltaTime;
+
+            if(Mathf.Abs(this.transform.position.x-target.transform.position.x)<5 && Mathf.Abs(this.transform.position.z-target.transform.position.z)<5)
+            {
+                targeting.PickedUp();
+                currentlyTargeting=false;
+            }
+        }
     }
-    }
+
+    
 
 
     public void SetTarget()
     {
         Debug.Log(this.transform.position + "hi");
         target=RockManager.Instance.FindClosesRock(this.transform);
-        RockPickedUp targeting = target.GetComponent<RockPickedUp>();
-        targeting.PickedUp();
+        targeting = target.GetComponent<RockPickedUp>();
+        //targeting.PickedUp(); //need to move first before pickup
+        
+        
+        transform.LookAt(target.transform);
+        currentlyTargeting=true;
     }
-}
+}//if close pick up
+//set currently targeting to false
