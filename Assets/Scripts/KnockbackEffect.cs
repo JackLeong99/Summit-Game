65906ba422edfe02eb926985b3edaff668bf19a8
Awoo_ <jackleong875@gmail.com>
2022-05-08 16:01:58 +1000

    Knockback changes
    Knockback is now relative to the root parent object rather than the hitbox its self

--- Assets/Scripts/KnockbackEffect.cs
+++ Assets/Scripts/KnockbackEffect.cs
@@ -4,11 +4,18 @@ using UnityEngine;
 
 public class KnockbackEffect : MonoBehaviour
 {
-    // Start is called before the first frame update
     [SerializeField] float force;
+
+    private Transform dealer;
+
+    private void Awake()
+    {
+        dealer = transform.root;      
+    }
+
     private void OnTriggerEnter(Collider collision)
     {
-        Vector3 dir = collision.transform.position - transform.position;
+        Vector3 dir = collision.transform.position - dealer.transform.position;
 
         KnockbackReciever reciever = collision.gameObject.GetComponent<KnockbackReciever>();
         if(reciever)
