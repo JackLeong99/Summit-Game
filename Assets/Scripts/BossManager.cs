0b17a3807fa25d3f1c9a13dcb2976097239aff17
Swirly <tian.heng@students.mq.edu.au>
2022-05-09 12:00:49 +1000

    Revert "Modified Boss Animator, Made new scene"
    This reverts commit b029e3ca7ced5c24373bd719fb60ff909884f9d5.

--- Assets/Scripts/BossManager.cs
+++ Assets/Scripts/BossManager.cs
@@ -27,44 +27,6 @@ public class BossManager : MonoBehaviour
     public int MoveSelector;
 
     public bool Rage;
-<<<<<<< Updated upstream
-=======
-    //shockwaveattack
-    //public GameObject shockwaveHitbox;
-    
-    //time between actions
-    public float delayBeforeNextAttack;
-    //to get variables from Shockwave
-    //public Shockwave ShockwaveScript;
-    //to prevent other actions starting while in one still
-    public bool inAttack = false;
-    //used to prevent range self-looping
-    public bool rangedAllowed = true;
-    //the timer for rangedAllowed
-    public float delayBeforeRangedAllowed;
-    public bool attackException = false;
-    public bool rage = false;
-
-    private BossPathing bPathing;
-    private tempShockwaveCaller shockwave;
-    private MegaPunch punch;
-    private GroundSlam slam;
-    private Eruption erupt;
-    private RockPathFinding rockThrow;
-    private new Animator animation;
-    
-
-    private void Awake(){
-        //defining other scripts referenceds them here- this method avoids an error.
-        bPathing = GetComponent<BossPathing>();
-        shockwave = GetComponent<tempShockwaveCaller>();
-        punch = GetComponent<MegaPunch>();
-        slam = GetComponent<GroundSlam>();
-        erupt = GetComponent<Eruption>();
-        rockThrow = GetComponent<RockPathFinding>();
-        animation = gameObject.GetComponent<Animator>();
-    }
->>>>>>> Stashed changes
     
     void Update(){
         GroundedCheck();
@@ -79,24 +41,6 @@ public class BossManager : MonoBehaviour
 
         //if not in melee
         if(Vector3.Distance(transform.position, Player.position) >= MinDist){
-<<<<<<< Updated upstream
-=======
-            //if(!inAttack){
-            bPathing.bossPathing();
-            animation.SetTrigger("Walk");
-            //}
-            //bPathing.GetComponent<BossPathing>().bossPathing();
-            //transform.Translate(transform.forward * MoveSpeed * Time.deltaTime);
-            //when doing a move pass SelectMove(Midattack1, Midattacklast);
-            bool isMidRange = Vector3.Distance(transform.position, Player.position) >= MinDist;
-            bool isLongRanged = Vector3.Distance(transform.position,Player.position) >= MaxDist;
-            if(isMidRange && !inAttack && !isLongRanged){
-                Debug.Log("Mid Range!");   
-            }                
-        }
-        
-        
->>>>>>> Stashed changes
         
             transform.Translate(transform.forward * MoveSpeed * Time.deltaTime);
             //when doing a move pass SelectMove(Midattack1, Midattacklast);
@@ -106,18 +50,7 @@ public class BossManager : MonoBehaviour
         // if in 'melee'
         if(Vector3.Distance(transform.position, Player.position) <= MinDist){
             //when doing a move pass SelectMove(Meleeattack1, Meleeattacklast); 
-<<<<<<< Updated upstream
 
-=======
-            //SelectMove(1, 1); //selectmove 1, last
-            //if(MoveSelector == 1){
-                //Instantiate(shockwaveHitbox, transform.position, transform.rotation);
-                //StartCoroutine(waitTime(2.3f, delayBeforeNextAttack));
-            Debug.Log("In melee!");
-            animation.SetTrigger("Idle");
-            StartCoroutine(meleeActions());    
-           // }
->>>>>>> Stashed changes
         }
 
         //If player is 'far' do 'ranged' 
@@ -126,96 +59,6 @@ public class BossManager : MonoBehaviour
         } 
     }
 
-<<<<<<< Updated upstream
-=======
-    IEnumerator meleeActions(){
-        //attack animation starts
-        inAttack = true;
-        SelectMove(1, 4);
-        //Shockwave
-        if(MoveSelector == 1){
-            //Debug.Log("Do Shockwave!");
-            //spawn the Shockwave Attack
-            shockwave.instantiateShockwave();
-            //Instantiate(shockwaveHitbox, transform.position, transform.rotation);
-            float animationDuration = 2;//ShockwaveScript.scaleTime;
-            yield return new WaitForSeconds(animationDuration + delayBeforeNextAttack);
-        }
-
-        //Mega Punch
-        if(MoveSelector == 2){
-            Debug.Log("Do MegaPunch!");
-            animation.SetTrigger("Sweep");
-            punch.megaPunch();
-            float animationDuration = 2; // Figure this out
-            yield return new WaitForSeconds(animationDuration + delayBeforeNextAttack);
-            
-        }
-
-        //Ground Slam
-        if(MoveSelector == 3){
-            Debug.Log("Do Ground Slam!");
-            animation.SetTrigger("Slam");
-            slam.groundSlam();
-            float animationDuration = 2; // Figure this out
-            yield return new WaitForSeconds(animationDuration + delayBeforeNextAttack);
-        }
-        //Coroutine finishes and boss is now able to select next action.
-        inAttack = false;
-        animation.SetTrigger("Idle");
-    }
-
-    IEnumerator midActions(){
-        inAttack = true;
-        SelectMove(3, 3);
-        yield return new WaitForSeconds(1);
-    }
-
-    IEnumerator rangedActions(){
-        inAttack = true;
-        rangedAllowed = false;
-        SelectMove(5,7);
-        //Ground Slam
-        if(MoveSelector == 5){
-            Debug.Log("Do Eruption!");
-            erupt.eruption();
-            animation.SetTrigger("Eruption");
-            float animationDuration = 2; // Figure this out
-           // float delayBeforeCurrentAttack = 1.5f; // Figure this out
-            yield return new WaitForSeconds(animationDuration + delayBeforeNextAttack);// + delayBeforeCurrentAttack
-        }
-        
-        //Rock Throw
-        if(MoveSelector == 6){
-            attackException = true;
-            Debug.Log("Do Rock Throw!");
-            rockThrow.SetTarget();
-            //while loop for wait for seconds- get isTargeting from RockPathFinding.
-            while(rockThrow.currentlyTargeting){
-                //bPathing.bossPathing();
-                yield return new WaitForSeconds(Time.deltaTime);
-            }
-            animation.SetTrigger("Throw");
-
-            attackException = false;
-
-            float animationDuration = 2; // Figure this out
-           // float delayBeforeCurrentAttack = 1.5f; // Figure this out
-            //while loop for wait for seconds- get isTargeting from RockPathFinding.
-            yield return new WaitForSeconds(animationDuration + delayBeforeNextAttack);// + delayBeforeCurrentAttack
-        }
-
-         
-         //Coroutine finishes and boss is now able to select next action. (including moving)
-        inAttack = false;
-        animation.SetTrigger("Idle");
-        //Prevents boss from spamming ranged attacks and locking itself into ranged animations - gives time to move forwards/advance
-        yield return new WaitForSeconds(delayBeforeRangedAllowed);
-        rangedAllowed = true;
-    }
-
-
->>>>>>> Stashed changes
     private void SelectMove(int min, int max){
         //if (Rage == true){ 
             //min+= Whatever the number of moves is;
