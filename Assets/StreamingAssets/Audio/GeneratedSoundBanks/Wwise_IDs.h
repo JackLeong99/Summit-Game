2e21a071b1bc1fdaaaa62e82b5761ff02821e109
Tofa <chris.eickelman@immutable.com>
2022-05-29 02:15:22 +1000

    Updated music. Set up structure & general skeleton of Wwise project.

---
+++ Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -0,0 +1,139 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Audiokinetic Wwise generated include file. Do not edit.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __WWISE_IDS_H__
+#define __WWISE_IDS_H__
+
+#include <AK/SoundEngine/Common/AkTypes.h>
+
+namespace AK
+{
+    namespace EVENTS
+    {
+        static const AkUniqueID AMBIENCE_PLAY = 1496310610U;
+        static const AkUniqueID AMBIENCE_STOP = 3166394572U;
+        static const AkUniqueID ENEMY_BOULDER_IMPACT = 2372917442U;
+        static const AkUniqueID ENEMY_BOULDER_THROW = 2364205524U;
+        static const AkUniqueID ENEMY_DEATH = 1205999388U;
+        static const AkUniqueID ENEMY_ERUPTION_BURST = 608829557U;
+        static const AkUniqueID ENEMY_ERUPTION_SLAM = 3010160010U;
+        static const AkUniqueID ENEMY_FOOTSTEPS = 3217092839U;
+        static const AkUniqueID ENEMY_MELEE_OVERHEAD_SLAM = 3324595863U;
+        static const AkUniqueID ENEMY_MELEE_RIGHT_HOOK = 1491778125U;
+        static const AkUniqueID GAME_QUIT = 2356404183U;
+        static const AkUniqueID GAME_START = 733168346U;
+        static const AkUniqueID MUSIC_DEFEAT = 3125733004U;
+        static const AkUniqueID MUSIC_PLAY = 202194903U;
+        static const AkUniqueID MUSIC_STOP = 3227181061U;
+        static const AkUniqueID MUSIC_VICTORY = 2637400697U;
+        static const AkUniqueID PLAYER_DAMAGE = 2074073782U;
+        static const AkUniqueID PLAYER_DODGE = 385316062U;
+        static const AkUniqueID PLAYER_FOOTSTEPS = 1730208058U;
+        static const AkUniqueID PLAYER_JUMP = 1305133589U;
+        static const AkUniqueID PLAYER_SHOOT_CAST = 3937919956U;
+        static const AkUniqueID PLAYER_SHOOT_IMPACT = 1650805379U;
+        static const AkUniqueID UI_CLICK = 2249769530U;
+        static const AkUniqueID UI_HIT_INDICATOR = 396309115U;
+        static const AkUniqueID UI_MENU_OFF = 537931389U;
+        static const AkUniqueID UI_MENU_ON = 1702827601U;
+        static const AkUniqueID VO_ENEMY_BOULDER_THROW = 3845949122U;
+        static const AkUniqueID VO_ENEMY_DEATH = 529689934U;
+        static const AkUniqueID VO_ENEMY_ERUPTION_SLAM = 526936832U;
+        static const AkUniqueID VO_ENEMY_IDLE = 3234914398U;
+        static const AkUniqueID VO_ENEMY_MELEE_OVERHEAD_SLAM = 1650187161U;
+        static const AkUniqueID VO_ENEMY_MELEE_RIGHT_HOOK = 3051830799U;
+        static const AkUniqueID VO_PLAYER_DAMAGE = 2922112884U;
+        static const AkUniqueID VO_PLAYER_IDLE = 3893789469U;
+        static const AkUniqueID VO_PLAYER_SPAWN = 3195722788U;
+        static const AkUniqueID VO_PLAYER_VICTORY = 4007178299U;
+    } // namespace EVENTS
+
+    namespace STATES
+    {
+        namespace UI_MENU
+        {
+            static const AkUniqueID GROUP = 2511555531U;
+
+            namespace STATE
+            {
+                static const AkUniqueID NONE = 748895195U;
+                static const AkUniqueID UI_MENU_OFF = 537931389U;
+                static const AkUniqueID UI_MENU_ON = 1702827601U;
+            } // namespace STATE
+        } // namespace UI_MENU
+
+    } // namespace STATES
+
+    namespace GAME_PARAMETERS
+    {
+        static const AkUniqueID MUSIC_SIDECHAIN = 266555239U;
+        static const AkUniqueID SFX_SIDECHAIN = 2862064063U;
+        static const AkUniqueID VO_SIDECHAIN = 2837652549U;
+        static const AkUniqueID VOLUME_MASTER = 3695994288U;
+        static const AkUniqueID VOLUME_MUSIC = 3891337659U;
+        static const AkUniqueID VOLUME_SFX = 3673881719U;
+        static const AkUniqueID VOLUME_VOICE = 3676256460U;
+    } // namespace GAME_PARAMETERS
+
+    namespace TRIGGERS
+    {
+        static const AkUniqueID MUSIC_DEFEAT_STINGER = 2515303183U;
+        static const AkUniqueID MUSIC_VICTORY_STINGER = 2143444826U;
+    } // namespace TRIGGERS
+
+    namespace BANKS
+    {
+        static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID SB_DEFAULT = 822237696U;
+    } // namespace BANKS
+
+    namespace BUSSES
+    {
+        static const AkUniqueID AMB_GAMEPLAY_LOOP = 2134809883U;
+        static const AkUniqueID ENEMY_BOULDER_IMPACT = 2372917442U;
+        static const AkUniqueID ENEMY_BOULDER_THROW = 2364205524U;
+        static const AkUniqueID ENEMY_DEATH = 1205999388U;
+        static const AkUniqueID ENEMY_ERUPTION_BURST = 608829557U;
+        static const AkUniqueID ENEMY_ERUPTION_SLAM = 3010160010U;
+        static const AkUniqueID ENEMY_FOOTSTEPS = 3217092839U;
+        static const AkUniqueID ENEMY_MELEE_OVERHEAD_SLAM = 3324595863U;
+        static const AkUniqueID ENEMY_MELEE_RIGHT_HOOK = 1491778125U;
+        static const AkUniqueID MASTER_AMBIENCE = 4104709401U;
+        static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+        static const AkUniqueID MASTER_MUSIC = 2318586166U;
+        static const AkUniqueID MASTER_SFX = 1078167454U;
+        static const AkUniqueID MASTER_VO = 1143233284U;
+        static const AkUniqueID MUSIC_DEFEAT_STINGER = 2515303183U;
+        static const AkUniqueID MUSIC_GAMEPLAY_LOOP = 1993100918U;
+        static const AkUniqueID MUSIC_VICTORY_STINGER = 2143444826U;
+        static const AkUniqueID PLAYER_ATTACK_LTR = 2800935486U;
+        static const AkUniqueID PLAYER_ATTACK_RTL = 3136193782U;
+        static const AkUniqueID PLAYER_DAMAGE = 2074073782U;
+        static const AkUniqueID PLAYER_DODGE = 385316062U;
+        static const AkUniqueID PLAYER_FOOTSTEPS = 1730208058U;
+        static const AkUniqueID PLAYER_JUMP = 1305133589U;
+        static const AkUniqueID PLAYER_SHOOT_CAST = 3937919956U;
+        static const AkUniqueID PLAYER_SHOOT_IMPACT = 1650805379U;
+        static const AkUniqueID SFX_ENEMY = 2297208207U;
+        static const AkUniqueID SFX_PLAYER = 217780010U;
+        static const AkUniqueID SFX_UI = 3862737079U;
+        static const AkUniqueID UI_CLICK = 2249769530U;
+        static const AkUniqueID UI_HIT_INDICATOR = 396309115U;
+        static const AkUniqueID UI_MENU_CLOSE = 3226991506U;
+        static const AkUniqueID UI_MENU_OPEN = 4083126854U;
+        static const AkUniqueID VO_ENEMY = 1837826845U;
+        static const AkUniqueID VO_PLAYER = 2966352764U;
+    } // namespace BUSSES
+
+    namespace AUDIO_DEVICES
+    {
+        static const AkUniqueID NO_OUTPUT = 2317455096U;
+        static const AkUniqueID SYSTEM = 3859886410U;
+    } // namespace AUDIO_DEVICES
+
+}// namespace AK
+
+#endif // __WWISE_IDS_H__
