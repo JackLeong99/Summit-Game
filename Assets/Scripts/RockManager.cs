f8e316d8f648cb0f6d8f941917bdcf62c65578ac
coolevil98 <odinbraad@gmail.com>
2022-06-04 15:15:30 +1000

    Updated rocks to come up from the ground

--- Assets/Scripts/RockManager.cs
+++ Assets/Scripts/RockManager.cs
@@ -124,28 +124,36 @@ public class RockManager : MonoBehaviour
     public void SpawnNewRocks()
     {
         GameObject newRock1= Instantiate(rockPrefab);
-        newRock1.transform.position= new Vector3(Random.Range(-50.0f, -25.0f), 0, Random.Range(-50.0f, 0f));
+        newRock1.transform.position= new Vector3(Random.Range(-50.0f, -25.0f), -2, Random.Range(-50.0f, 0f));
+        newRock1.GetComponent<RockPickedUp>().SpawnUp();
         allRocks.Add(newRock1);
         GameObject newRock2= Instantiate(rockPrefab);
-        newRock2.transform.position= new Vector3(Random.Range(-50.0f, -25.0f), 0, Random.Range(0f, 50f));
+        newRock2.transform.position= new Vector3(Random.Range(-50.0f, -25.0f), -2, Random.Range(0f, 50f));
+        newRock2.GetComponent<RockPickedUp>().SpawnUp();
         allRocks.Add(newRock2);
         GameObject newRock3= Instantiate(rockPrefab);
-        newRock3.transform.position= new Vector3(Random.Range(-25.0f, 0f), 0, Random.Range(-50f, 0f));
+        newRock3.transform.position= new Vector3(Random.Range(-25.0f, 0f), -2, Random.Range(-50f, 0f));
+        newRock3.GetComponent<RockPickedUp>().SpawnUp();
         allRocks.Add(newRock3);
         GameObject newRock4= Instantiate(rockPrefab);
-        newRock4.transform.position= new Vector3(Random.Range(-25.0f, 0f), 0, Random.Range(0f, 50f));
+        newRock4.transform.position= new Vector3(Random.Range(-25.0f, 0f), -2, Random.Range(0f, 50f));
+        newRock4.GetComponent<RockPickedUp>().SpawnUp();
         allRocks.Add(newRock4);
         GameObject newRock5= Instantiate(rockPrefab);
-        newRock5.transform.position= new Vector3(Random.Range(0f, 25f), 0, Random.Range(-50f, 0f));
+        newRock5.transform.position= new Vector3(Random.Range(0f, 25f), -2, Random.Range(-50f, 0f));
+        newRock5.GetComponent<RockPickedUp>().SpawnUp();
         allRocks.Add(newRock5);
         GameObject newRock6= Instantiate(rockPrefab);
-        newRock6.transform.position= new Vector3(Random.Range(0f, 25f), 0, Random.Range(0f, 50f));
+        newRock6.transform.position= new Vector3(Random.Range(0f, 25f), -2, Random.Range(0f, 50f));
+        newRock6.GetComponent<RockPickedUp>().SpawnUp();
         allRocks.Add(newRock6);
         GameObject newRock7= Instantiate(rockPrefab);
-        newRock7.transform.position= new Vector3(Random.Range(25f, 50f), 0, Random.Range(-50f, 0f));
+        newRock7.transform.position= new Vector3(Random.Range(25f, 50f), -2, Random.Range(-50f, 0f));
+        newRock7.GetComponent<RockPickedUp>().SpawnUp();
         allRocks.Add(newRock7);
         GameObject newRock8= Instantiate(rockPrefab);
-        newRock8.transform.position= new Vector3(Random.Range(25f, 50f), 0, Random.Range(0f, 50f));
+        newRock8.transform.position= new Vector3(Random.Range(25f, 50f), -2, Random.Range(0f, 50f));
+        newRock8.GetComponent<RockPickedUp>().SpawnUp();
         allRocks.Add(newRock8);
     }
 
