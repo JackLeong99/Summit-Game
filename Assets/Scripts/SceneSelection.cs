0290ffdaaef53b896a0743b0622fc18b888cc24a
coolevil98 <odinbraad@gmail.com>
2022-08-24 11:08:05 +1000

    Created method to determine which scene is next

---
+++ Assets/Scripts/SceneSelection.cs
@@ -0,0 +1,73 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneSelection : MonoBehaviour
+{
+
+    string[] avaliableLevels = { "AnimatorAttempt", "Reaper", "Archer", "Mage"}; //each one is a level name
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    if(Input.GetButtonDown("Escape"))
+    {
+        SelectNewScene();
+    }
+    }
+
+    //put all in array then each time do random select and then remove it from the array
+
+    public void SelectNewScene()
+    {
+        int nextLevel =Random.Range(0, avaliableLevels.Length);
+        Debug.Log(avaliableLevels.Length);
+        string nextLevelString=avaliableLevels[nextLevel];
+        string[] tempAvaliableLevels=new string[avaliableLevels.Length-1];
+        int tracker=0;
+        for( int i=0; i<avaliableLevels.Length; i++)
+        {
+            if(avaliableLevels[i]!=nextLevelString)
+            {
+                tempAvaliableLevels[i-tracker]=avaliableLevels[i];
+            }
+            if(avaliableLevels[i]==nextLevelString)
+            {
+            tracker=1;
+            }
+        }   
+        avaliableLevels=new string[tempAvaliableLevels.Length];
+        avaliableLevels=tempAvaliableLevels;
+        Debug.Log(avaliableLevels.Length);
+    }
+
+    //create method that sends to each scene, then random select what scenes and make array that will be refered to each time
+
+    //These methods are used for testing purposes ie when we get people to playtest
+    public void RockScene()
+    {
+        SceneManager.LoadScene("AnimatorAttempt");
+    }
+    public void ReaperScene()
+    {
+        SceneManager.LoadScene("AnimatorAttempt"); //to be changed
+    }
+    public void MageScene()
+    {
+        SceneManager.LoadScene("AnimatorAttempt"); //to be changed
+    }
+    public void ArcherScene()
+    {
+        SceneManager.LoadScene("AnimatorAttempt"); //to be changed
+    }
+    public void SecretScene()
+    {
+        SceneManager.LoadScene("AnimatorAttempt"); //to be changed
+    }
+}
