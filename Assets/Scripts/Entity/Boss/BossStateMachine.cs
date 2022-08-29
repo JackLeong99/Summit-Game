27c26e9f942422d16cabab424fb7c19b85ade277
Awoo_ <jackleong875@gmail.com>
2022-08-29 16:55:27 +1000

    Fixes to DoT

--- Assets/Scripts/Entity/Boss/BossStateMachine.cs
+++ Assets/Scripts/Entity/Boss/BossStateMachine.cs
@@ -127,12 +127,6 @@ public class BossStateMachine : MonoBehaviour
 
     public void TakeDamage(float[] dmg, float tickRate, Vector3 position)
     {
-        switch (components.iFrame)
-        {
-            case IState.Active:
-                return;
-        }
-
         StartCoroutine(IFrame());
 
         StartCoroutine(TickDamage(dmg, tickRate, position));
--- Assets/Scripts/Pojectile system/OnHitEffects/BossDoT.cs
+++ Assets/Scripts/Pojectile system/OnHitEffects/BossDoT.cs
@@ -6,30 +6,14 @@ using UnityEngine;
 
 public class BossDoT : OnHitEffect
 {
-    [Tooltip("Time DoT is applied in seconds")]
-    public float time;
     [Tooltip("Number of seconds between damage ticks")]
     public float rate;
     [Tooltip("Damage per tick")]
-    public float damage;
+    public float[] damage;
 
     public override void ApplyOnHitEffects(GameObject target)
     {
-        target.GetComponent<EnemyDamageReceiver>().StartCoroutine(DoT(target));
+        target.GetComponent<EnemyDamageReceiver>().PassDamage(damage, rate, target.transform.position);
     }
 
-    IEnumerator DoT(GameObject target) 
-    {
-        float currentTime = time;
-        EnemyDamageReceiver boss = target.GetComponent<EnemyDamageReceiver>();
-
-        while (currentTime > 0) 
-        {
-            boss.PassDamage(damage, boss.transform.position);
-            currentTime -= rate;
-            yield return new WaitForSeconds(rate);
-        }
-
-        yield return null;
-    }
 }
