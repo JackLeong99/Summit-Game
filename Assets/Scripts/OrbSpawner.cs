dd040fa4a19a4f49a92d3c3c143ac4abfb179cde
coolevil98 <odinbraad@gmail.com>
2022-08-31 15:57:31 +1000

    Begun to create orbs for sandstorm

---
+++ Assets/Scripts/OrbSpawner.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OrbSpawner : MonoBehaviour
+{
+    public GameObject orbPrefab;
+    public int minXPos=0;
+    public int maxXPos=50;
+    public int minZPos=0;
+    public int maxZPos=50;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetButtonDown("Teleport"))
+        {
+            OrbCreation();
+        }
+    }
+
+
+    //this is called by sandstorm or moved into sandstorm script
+    public void OrbCreation()
+    {
+        for(int i=0; i<3; i++)
+        {
+            int xPos=Random.Range(minXPos, maxXPos);
+            int zPos=Random.Range(minZPos, maxZPos);
+       // GameObject orb = 
+            Instantiate(orbPrefab,new Vector3(xPos, 3, zPos),Quaternion.identity);
+        }
+        //50 by 50
+        //when setting up scene location of oasis will need to be inputted into this
+    }
+
+    //need to create function to see how many orbs are left
+    //need script for orb themself
+}
