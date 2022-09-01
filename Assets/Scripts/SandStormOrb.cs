10a5fb7a36c0b14dce7add3e1459e23b8fe7b6bd
coolevil98 <odinbraad@gmail.com>
2022-09-01 13:44:19 +1000

    created sandstorm orb
    All that is left is orb and sandstorm reference

--- Assets/Scripts/SandStormOrb.cs
+++ Assets/Scripts/SandStormOrb.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class SandStormOrb : MonoBehaviour
 {
+    public int orbHealth=5;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,4 +18,15 @@ public class SandStormOrb : MonoBehaviour
     }
     //needs health
     //something to disable sandstorm
+    private void OnTriggerEnter(Collider other){
+        if(other.tag=="sword" || other.tag == "PlayerBullet")
+        {
+            orbHealth--;
+            if(orbHealth<=0)
+            {
+                //disable sandstorm
+                Destroy(gameObject);
+            }
+        }
+    }
 }
