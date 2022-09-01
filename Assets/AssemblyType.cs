34fe03d1283aa79a4a85253f931b77b547a38bb4
Aiden Nathan <agent9@y7mail.com>
2022-09-02 01:47:49 +1000

    State Compile & Scriptable Editor Added

---
+++ Assets/AssemblyType.cs
@@ -0,0 +1,9 @@
+﻿using System.Reflection;
+
+public static class AssemblyTypes
+{
+    public static System.Type[] ReturnTypes()
+    {
+        return Assembly.GetExecutingAssembly().GetTypes();
+    }
+}
