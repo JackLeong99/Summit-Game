a0169f78c5dae3aa5e27c793a51753de425a29ec
Aiden Nathan <agent9@y7mail.com>
2022-09-29 06:04:12 +1000

    Modularized Entity Pathing for AbilityStates
    - Golem rock throw chain 2/3 implemented
    -  Modular Entity pathing implemented

---
+++ Assets/Scripts/Entity/Boss/Bosses/Bases/Abilities/EntityPathState.cs
@@ -0,0 +1,68 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Boss/Ability/Golem/Entity Pathing State")]
+public class EntityPathState : AbilityState
+{
+    [Header("Values")]
+    public string targetTag;
+    public float stoppingDist;
+
+    public override void Invoke(BossStateMachine boss)
+    {
+        base.Invoke(boss);
+
+        Setup();
+    }
+
+    public override void Update()
+    {
+        base.Update();
+
+        switch (true)
+        {
+            case bool x when Vector3.Distance(boss.agent.destination, boss.transform.position) < stoppingDist:
+                animationActive = AnimationState.Done;
+                break;
+        }
+    }
+
+    public override void Exit()
+    {
+        base.Exit();
+
+        boss.agent.isStopped = true;
+        boss.agent.destination = GameManager.instance.player.transform.position;
+    }
+
+    public override void Setup()
+    {
+        boss.agent.destination = GetNearestEntity().transform.position;
+        boss.agent.isStopped = false;
+    }
+
+    public GameObject GetNearestEntity()
+    {
+        GameObject[] entities = GameObject.FindGameObjectsWithTag(targetTag);
+        float distance = 0;
+        int index = 0;
+
+        for (int i = 0; i < entities.Length; i++)
+        {
+            float currentDist = Vector3.Distance(entities[i].transform.position, boss.transform.position);
+
+            switch (true)
+            {
+                case bool x when i == 0:
+                case bool y when currentDist < distance:
+                    distance = currentDist;
+                    index = i;
+                    break;
+            } 
+        }
+
+        return entities[index];
+    }
+}
