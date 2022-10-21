6b0cc7f5793ea80aaffc427b18e4b411ddf5c132
Awoo_ <jackleong875@gmail.com>
2022-10-21 18:08:32 +1100

    added proper weakpoints to golem + missile tracking improvements

--- Assets/Scripts/Entity/Hitboxes/EnemyDamageReceiver.cs
+++ Assets/Scripts/Entity/Hitboxes/EnemyDamageReceiver.cs
@@ -9,6 +9,8 @@ public class EnemyDamageReceiver : MonoBehaviour
     public GameObject onHitParticles;
     public static event Action OnDamageTaken;
     public bool weakSpot;
+    public float stunLockoutTime;
+    private bool stunLockout;
     
     void Start() 
     {
@@ -18,7 +20,7 @@ public class EnemyDamageReceiver : MonoBehaviour
     public void PassDamage(float dmg, Vector3 position)
     {
         if (!boss.Alive()) return;
-        if (weakSpot) boss.components.stunState = StunState.Stunned;
+        if (weakSpot) StartCoroutine(tryStun());
         boss.TakeDamage(dmg, position);
         DamageHandler();
     }
@@ -26,9 +28,23 @@ public class EnemyDamageReceiver : MonoBehaviour
     public void PassDamage(float[] dmg, float tickRate, Vector3 position)
     {
         if (!boss.Alive()) return;
-        if (weakSpot) boss.components.stunState = StunState.Stunned;
+        //if (weakSpot) boss.components.stunState = StunState.Stunned;
         boss.TakeDamage(dmg, tickRate, position);
         DamageHandler();
+    }
+
+    public IEnumerator tryStun() 
+    {
+        if (!stunLockout && boss.components.stunState != StunState.Stunned) 
+        {
+            boss.components.stunState = StunState.Stunned;
+            GetComponent<MeshRenderer>().material.DisableKeyword("_EMISSION");
+            stunLockout = true;
+            yield return new WaitForSeconds(stunLockoutTime);
+            GetComponent<MeshRenderer>().material.EnableKeyword("_EMISSION");
+            stunLockout = false;
+        }
+        yield return null;
     }
 
     public void DamageHandler()
