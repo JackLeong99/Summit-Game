cedefbd0e30be88c65a572da740e4f2e3df87b7c
Aiden Nathan <agent9@y7mail.com>
2022-10-29 04:02:22 +1100

    Scriptable Editor Changes

---
+++ Assets/Editor/ScriptableObjectEditor/Resources/TemplateScriptable.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Default Name")]
+public class TemplateScriptable : ScriptableObject
+{
+    public void Awake()
+    {
+        
+    }
+}
--- Assets/Editor/ScriptableObjectEditor/Scripts/NamingEditorWindow.cs
+++ Assets/Editor/ScriptableObjectEditor/Scripts/NamingEditorWindow.cs
@@ -5,16 +5,20 @@ public class NamingEditorWindow : PopupWindowContent
 {
     protected string newName = "Please enter a new name";
     protected Object selectedObject;
-    protected Rect button;
+    protected Rect position;
+
+    protected Vector2 dimensions = new Vector2(300, 125);
 
     public NamingEditorWindow(Object selected, Rect origin)
     {
         selectedObject = selected;
-        button = origin;
+        position = origin;
     }
 
     public override void OnGUI(Rect rect)
     {
+        editorWindow.position = AssemblyTypes.CenterOnOriginWindow(editorWindow.position, position);
+
         EditorGUILayout.Space(10);
         var style = new GUIStyle(GUI.skin.label);
         style.fontSize = 12;
@@ -41,6 +45,6 @@ public class NamingEditorWindow : PopupWindowContent
 
     public override Vector2 GetWindowSize()
     {
-        return new Vector2(button.width, 125);
+        return dimensions;
     }
 }
